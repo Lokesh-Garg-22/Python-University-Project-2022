27f21491775b6b6fc6ba55989f671c112b368c74
Lokesh Garg <lokeshgargadv@gmail.com>
2022-12-13 22:34:19 +0530

    Edit

--- Classes.py
+++ Classes.py
@@ -1,12 +1,12 @@
-import random
+import random, json
 
 class Item:
-    discription = ""
-
-    def __init__(self, name, price, discount = 0):
+    def __init__(self, name, price, discount = 0, id=0, discription = ""):
+        self.id = id
         self.name = name
         self.price = price
         self.discount = discount
+        self.discription = discription
 
     def __repr__(self):
         return self.name
@@ -41,10 +41,20 @@ class Menu:
                 result += ("{}".format(j).ljust(40) + "{}\n".format(j.get_cost()))
         return result
 
+    def generate_item_id(self):
+        max_id = 0
+        for i in self.groups:
+            for j in self.groups[i]["items"]:
+                if j.id > max_id:
+                    max_id = j.id
+        id = max_id + 1
+        return id
+
     def add_item(self, item, group):
         if type(item) != Item:
             raise Exception(item, "is not an Item.")
             return
+        item.id = self.generate_item_id()
         if group in self.groups:
             self.groups[group]["items"].append(item)
         else:
@@ -66,9 +76,11 @@ class Menu:
 
 class Cafe:
     data_file = "data.json"
+    data_back_up_file = "data_back_up.json"
     menu = None
     users = []
     coupons = {}
+    locations = []
 
     def __init__(self, name):
         self.name = name
@@ -84,11 +96,20 @@ class Cafe:
             return self.menu
         else:
             return self.menu.print_group(group)
+
+    def generate_user_id(self):
+        max_id = 0
+        for i in self.users:
+            if i.id > max_id:
+                max_id = i.id
+        id = max_id + 1
+        return id
     
     def add_user(self, user):
         if type(user) != User:
             raise Exception(user, "is not a User.")
             return
+        user.id = self.generate_user_id()
         self.users.append(user)
 
     def generate_coupon_id(self, length = 10):
@@ -116,21 +137,111 @@ class Cafe:
     def add_coupon(self, id, discount, times_used = 1):
         self.coupons[id] = {"discount": discount, "times_used": times_used}
 
-    def load_data(self):
-        pass
+    def add_locations(self, *location):
+        for l in location:
+            self.locations.append(l)
 
-    def save_data(self):
-        pass
+    def load_back_up_data(self):
+        try:
+            with open(self.data_back_up_file, 'r') as data_file:
+                data = json.load(data_file)
+            self.load_data(data)
+        except:
+            return "BackUp not Found"
+
+    def load_data(self, data = None):
+        if data == None:
+            try:
+                with open(self.data_file, 'r') as data_file:
+                    data = json.load(data_file)
+            except:
+                data = {
+                        "coupons": {},
+                        "locations": [],
+                        "users": [],
+                        "menu": {}
+                    }
+
+        self.coupons = data.get("coupons", {})
+        self.locations = data.get("locations", [])
+
+        #Load Menu Data
+        self.set_menu(Menu())
+        for i in data.get("menu", {}):
+            for j in data["menu"][i]["items"]:
+                self.menu.add_item(Item(**j), i)
+        
+        #Load User Data
+        self.users = []
+        for i in data.get("users", []):
+            o = []
+            for j1 in i["orders"]:
+                for j2 in self.menu.groups:
+                    for j3 in self.menu.groups[j2]["items"]:
+                        if j1["item"] == j3.id:
+                            o.append({"item": j3, "quantity": j1["quantity"]})
+            i["orders"] = o
+            self.add_user(User(**i))
+
+        return "Data Loaded Successfully"
+
+    def save_back_up_data(self):
+        data = self.save_data(return_data=True)
+        with open(self.data_back_up_file, "w") as data_file:
+            data_file.write(json.dumps(data, indent=4))
+        return "BackUp Data Saved Successfully"
+
+    def save_data(self, return_data = False):
+        data = {
+            "coupons": self.coupons,
+            "locations": self.locations
+        }
+
+        #Save Menu Data
+        t = {}
+        for i in self.menu.groups:
+            tt = {}
+            tt["items"] = []
+            for j in self.menu.groups[i]["items"]:
+                ttt = {}
+                ttt["id"] = j.id
+                ttt["name"] = j.name
+                ttt["price"] = j.price
+                ttt["discount"] = j.discount
+                ttt["discription"] = j.discription
+                tt["items"].append(ttt)
+            t[i] = (tt)
+        data["menu"] = t
+
+        #Save Users Data
+        t = []
+        for i in self.users:
+            tt = {}
+            tt["id"] = i.id
+            tt["name"] = i.name
+            tt["password"] = i.password
+            tt["admin"] = i.admin
+            tt["location"] = i.location
+            tt["orders"] = [{"item": j["item"].id, "quantity": j["quantity"]} for j in i.orders]
+            t.append(tt)
+        data["users"] = t
+
+        with open(self.data_file, "w") as data_file:
+            data_file.write(json.dumps(data, indent=4))
+        if return_data:
+            return data
+        else:
+            return "Data Saved Successfully"
 
 
 class User:
-    orders = []
-
-    def __init__(self, name, password, admin = False, location = "offline"):
+    def __init__(self, name, password, admin = False, location = "offline", id = 0, orders = []):
+        self.id = id
         self.name = name
         self.password = password
         self.admin = admin
         self.location = location
+        self.orders = orders
     
     def __repr__(self):
         return self.name
@@ -140,3 +251,9 @@ class User:
             return True
         else:
             return False
+
+    def add_order(self, item, quantity = 1):
+        if type(item) != Item:
+            raise Exception(item, "is not an Item")
+            return
+        self.orders.append({"item": item, "quantity": quantity})
--- Main-(LAB_2).py
+++ Main-(LAB_2).py
@@ -1,15 +1,25 @@
 from Classes import *
 
-cafe = Cafe("Elite Cafe")
-menu = Menu()
-menu.add_item(Item("Item-1",20),"Group-1")
-menu.add_item(Item("Item-2",20),"Group-1")
-menu.add_item(Item("Item-3",20,10),"Group-1")
-menu.add_item(Item("Item-4",20),"Group-2")
-menu.add_item(Item("Item-5",20),"Group-2")
-cafe.set_menu(menu)
+def reset_cafe(cafe = Cafe("")):
+    if type(cafe) != Cafe:
+        raise Exception(cafe, "is not a Cafe")
+        return
+    cafe.set_menu(Menu())
+    cafe.menu.add_item(Item("Item-1",20),"Group-1")
+    cafe.menu.add_item(Item("Item-2",20),"Group-1")
+    cafe.menu.add_item(Item("Item-3",20,10),"Group-1")
+    cafe.menu.add_item(Item("Item-4",20),"Group-2")
+    cafe.menu.add_item(Item("Item-5",20),"Group-2")
+    cafe.generate_coupon(10)
+    cafe.add_user(User("Admin", "admin", True))
+    cafe.save_data()
 
-print(cafe.print_menu())
-print(cafe.print_menu("Group-1"))
-cafe.generate_coupon(10)
+
+cafe = Cafe("Elite Cafe")
+cafe.load_data()
+
+print(cafe.menu)
 print(cafe.coupons)
+print(cafe.users)
+print(cafe.users[0].id)
+print(cafe.users[0].orders)
